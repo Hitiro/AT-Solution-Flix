39538473d9199a52ebd57333fbec42e393cc0de4
Alberto Hitiro Tsugawa <hitiro@live.com>
2022-12-16 10:17:03 -0300

    feat: adicionando detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,65 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`,
+        {
+          params: {
+            api_key: "673345f12c42415b28b7b1e0eb56c22d",
+            language: "pt-BR",
+          }
+        })
+
+        .then((response) => {
+          setFilme(response.data);
+          setIsLoading(false);
+          // console.log(response.data);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+        })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("Componente desmontado");
+    }
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="filme-info">
+        <h1>
+          Carregando detalhes do filme ...
+        </h1>
+      </div>
+    )
+  }
+
   return (
-    <div>BEM VINDO DETALHES DO FILME</div>
+    <div className="filme-info">
+
+      <h1>{filme.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+    </div>
   )
+
 }
 
 export default Filme;
